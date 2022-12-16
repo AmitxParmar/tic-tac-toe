28cdcd066e0022280a84cfeb7437c872d5c965ba
AmitxParmar <amitparmar901@gmail.com>
2022-12-16 20:40:01 +0530

    done with initial stage(logic)

---
+++ components/Square.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+const Square = ({
+    value, onClick, winner
+}: {
+    winner: string | null
+    value: string | null
+    onClick: () => void;
+
+}) => {
+
+    if (!value) {
+        return <button className='square' onClick={onClick} disabled={Boolean(winner)} />;
+    }
+    return (
+        <button className={`square square_${value.toLowerCase}`} disabled > {value}</button >
+    )
+}
+
+export default Square
---
+++ containers/Board.tsx
@@ -0,0 +1,40 @@
+import React, { SetStateAction, useState } from 'react'
+import Square from '../components/Square';
+
+function Board() {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const val = Math.round(Math.random() * 1) === 1 ? "X" : "O";
+    const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">(val);
+
+    const [winner, setWinner] = useState(null);
+
+    function setSquareValue(index: number) {
+        const newData = squares.map((val, i) => {
+            if (i === index) {
+                return currentPlayer;
+            }
+            return val;
+        });
+        setSquares(newData)
+        setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+    }
+
+    return (
+        <div>
+            <p>Hey {currentPlayer}, it's your turn</p>
+            <div className='grid'>
+                {
+                    Array(9).fill(null).map((_, i) => {
+                        return <Square
+                            winner={winner}
+                            key={i}
+                            onClick={() => setSquareValue(i)}
+                            value={squares[i]}
+                        />
+                    })}
+            </div>
+        </div>
+    )
+}
+
+export default Board
